6d73790491b09c594c3be6b8af3515afbd956b3a
Tejas Narayan <tejas.k.narayan@gmail.com>
2025-12-06 23:31:21 -0500

    solved day 6

--- 6/solution.py
+++ 6/solution.py
@@ -1,33 +1,9 @@
-# /usr/bin/env python3
+#/usr/bin/env python3
 
 from sys import argv
-from functools import reduce
+
+file_in = open(argv[1] if len(argv) > 1 else 'input.txt', 'r')
 
 
-def str_at(s, i):
-    if i >= len(s):
-        return ""
-    return s[i]
 
-
-with open(argv[1] if len(argv) > 1 else "input.txt", "r") as file_in:
-    lines = file_in.read().splitlines()
-
-rows = []
-ops = lines[-1].split()
-
-for line in lines[:-1]:
-    rows.append(line.split())
-
-cols = list(zip(*rows))
-
-total = 0
-corrected = 0
-
-cols = [list(map(int, col)) for col in cols]
-
-for op, col in zip(ops, cols):
-    total += sum(col) if op == "+" else reduce(lambda x, y: x * y, col)
-
-print("   answer total:", total)
-print("corrected total:", corrected)
+file_in.close()

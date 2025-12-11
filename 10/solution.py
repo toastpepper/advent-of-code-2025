9054167ae562acb40dcaea190a7bf74ebce372d9
Tejas Narayan <tejas.k.narayan@gmail.com>
2025-12-11 11:41:09 -0500

    what the hell is a z3

--- 10/solution.py
+++ 10/solution.py
@@ -1,6 +1,7 @@
 # /usr/bin/env python3
 
 from sys import argv
+import z3
 
 with open(argv[1] if len(argv) > 1 else "input.txt", "r") as file_in:
     machines = [line.split() for line in file_in.readlines()]
@@ -12,8 +13,9 @@ for i, m in enumerate(machines):
     j = [int(n) for n in m[-1][1:-1].split(",")]
     machines[i] = (l, b, j)
 
-total_steps = 0
-for schematic, buttons, _ in machines:
+light_steps = 0
+joltage_steps = 0
+for light_target, buttons, joltage_target in machines:
     min_steps = None
     for i in range(2 ** len(buttons)):
         selection = set()
@@ -22,18 +24,40 @@ for schematic, buttons, _ in machines:
             if (i >> j) % 2:
                 selection.add(j)
 
-        lights = [False] * len(schematic)
+        lights = [False] * len(light_target)
 
         # press buttons
         for ind in selection:
             for light in buttons[ind]:
                 lights[light] = not lights[light]
 
-        if lights == schematic:
+        if lights == light_target:
             steps = len(selection)
             if min_steps is None:
                 min_steps = steps
             else:
                 min_steps = min(steps, min_steps)
-    total_steps += min_steps
-print("light configuration steps:", total_steps)
+    light_steps += min_steps
+
+    presses = z3.IntVector("x", len(buttons))
+    s = z3.Solver()
+    s.add([x >= 0 for x in presses])
+    # each joltage must equal the sum of presses for buttons that include it
+    for i, joltage in enumerate(joltage_target):
+        to_press = []
+        for j, button in enumerate(buttons):
+            if i in button:
+                to_press.append(presses[j])
+        s.add(z3.Sum(to_press) == joltage)
+
+    for i in range(1000):
+        s.push()
+        s.add(z3.Sum(presses) == i)
+        if s.check() == z3.sat:
+            joltage_steps += i
+            break
+        s.pop()
+
+
+print("  light steps:", light_steps)
+print("joltage steps:", joltage_steps)
